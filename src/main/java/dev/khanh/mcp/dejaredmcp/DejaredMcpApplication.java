b2ee221651448384067f266c0f5d1c4b3cf49b5d
HuynhKhanh1402 <khanh@duck.com>
2026-04-01 08:41:56 +0700

    chore: add ASM, CFR, Vineflower, Procyon dependencies and MCP STDIO config

---
+++ src/main/java/dev/khanh/mcp/dejaredmcp/DejaredMcpApplication.java
@@ -0,0 +1,13 @@
+package dev.khanh.mcp.dejaredmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DejaredMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DejaredMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/khanh/mcp/dejaredmcp/DejaredMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.khanh.mcp.dejaredmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DejaredMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
